b7d9bda8973a0c2063f18eee4e5c8d6c01f0ca43
abdelrahman mostafa <abdelrahman.sheta16@gmail.com>
2023-11-19 18:45:54 +0200

    Update DAO functionalities

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -1,1 +1,9 @@
+// contracts/Box.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Box is Ownable {
+    uint256 private value;
+}
