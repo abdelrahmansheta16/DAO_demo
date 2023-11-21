4139b3c9fc5fcf0ce666208e811212355d4a8c3d
abdelrahman mostafa <abdelrahman.sheta16@gmail.com>
2023-11-21 18:54:25 +0200

    Update DAO functionalities

--- deploy/04-setup-governance-contracts.ts
+++ deploy/04-setup-governance-contracts.ts
@@ -1,1 +1,4 @@
-
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { ethers } from "hardhat";
+import { ADDRESS_ZERO } from "../hardhat-helper-config";
