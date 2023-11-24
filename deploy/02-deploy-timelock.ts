e8fccee3aca52fc663ee675705492471a3823a03
abdelrahman mostafa <abdelrahman.sheta16@gmail.com>
2023-11-24 18:50:43 +0200

    Update DAO functionalities

--- deploy/02-deploy-timelock.ts
+++ deploy/02-deploy-timelock.ts
@@ -1,2 +1,11 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { EXECUTORS, MIN_DELAY, PROPOSERS } from "../hardhat-helper-config";
 
-
+const deployTimeLock: DeployFunction = async (
+  hre: HardhatRuntimeEnvironment
+) => {
+  const { getNamedAccounts, deployments, network } = hre;
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+};
