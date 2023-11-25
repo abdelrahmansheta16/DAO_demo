51ea777336e7a1ed8bfb108961467ad998bc2b7c
abdelrahman mostafa <abdelrahman.sheta16@gmail.com>
2023-11-25 18:56:42 +0200

    Update DAO functionalities

--- deploy/05-deploy-box-contract.ts
+++ deploy/05-deploy-box-contract.ts
@@ -1,3 +1,11 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 import { ethers } from "hardhat";
+
+const deployBox: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+  const { getNamedAccounts, deployments, network } = hre;
+  const { deployer } = await getNamedAccounts();
+  const { deploy, log } = deployments;
+
+  log("Deploying 'Box' Contract....");
+};
