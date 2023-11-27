2200872b1d3ff769a892aa51e4241d3b6456b642
abdelrahman mostafa <abdelrahman.sheta16@gmail.com>
2023-11-27 18:47:49 +0200

    Update DAO functionalities

--- contracts/GovernanceToken.sol
+++ contracts/GovernanceToken.sol
@@ -26,4 +26,11 @@ contract GovernanceToken is ERC20Votes {
 	function _mint(address to, uint256 amount) internal override(ERC20Votes) {
 		super._mint(to, amount);
 	}
+
+	function _burn(address account, uint256 amount)
+		internal
+		override(ERC20Votes)
+	{
+		super._burn(account, amount);
+	}
 }
