3a04666dec2a282bff2ba4dc97f63f4cfd1040ac
abdelrahman mostafa <abdelrahman.sheta16@gmail.com>
2023-11-28 18:52:02 +0200

    Update DAO functionalities

--- deploy/03-deploy-governor-contract.ts
+++ deploy/03-deploy-governor-contract.ts
@@ -1,1 +1,7 @@
-
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import {
+  QUORUM_PERCENTAGE,
+  VOTING_DELAY,
+  VOTING_PERIOD,
+} from "../hardhat-helper-config";
