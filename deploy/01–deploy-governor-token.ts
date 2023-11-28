ce695912e09bce1367cea419e3a6c2634bdf76cf
abdelrahman mostafa <abdelrahman.sheta16@gmail.com>
2023-11-28 18:48:04 +0200

    Update DAO functionalities

---
+++ deploy/01–deploy-governor-token.ts
@@ -0,0 +1,12 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { ethers } from "hardhat";
+
+const deployGovernanceToken: DeployFunction = async (
+  hre: HardhatRuntimeEnvironment
+) => {
+  const { getNamedAccounts, deployments, network } = hre;
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+};
+
