50f053c52e6f580a2d80b88af171ea7338b16146
abdelrahman mostafa <abdelrahman.sheta16@gmail.com>
2023-12-01 19:02:37 +0200

    Update DAO functionalities

--- scripts/vote.ts
+++ scripts/vote.ts
@@ -1,1 +1,9 @@
-
+import { ethers, network } from "hardhat";
+import {
+  developmentChains,
+  PROPOSAL_FILE,
+  VOTE_REASON,
+  VOTING_PERIOD,
+} from "../hardhat-helper-config";
+import * as fs from "fs";
+import { moveBlocks } from "../utils/timetravel";
